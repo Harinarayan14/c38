e9c6918e45287294a342ee2266b302d7a9eae035
harinarayan14 <gjayashankar2008@gmail.com>
2020-05-06 16:57:33 +0530

    class completed

--- js/Game.js
+++ js/Game.js
@@ -30,9 +30,13 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage(car1Image);
     car2 = createSprite(300,200);
+    car2.addImage(car2Image);
     car3 = createSprite(500,200);
+    car3.addImage(car3Image);
     car4 = createSprite(700,200);
+    car4.addImage(car4Image);
     cars = [car1, car2, car3, car4];
   }
 
@@ -43,12 +47,13 @@ class Game {
     
     if(allPlayers !== undefined){
       //var display_position = 100;
-      
+      background(groundImage);
+      image(trackImage,0,-displayHeight*4,displayWidth,displayHeight*5);
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 175;
       var y;
 
       for(var plr in allPlayers){
@@ -74,11 +79,18 @@ class Game {
 
     }
 
-    if(keyIsDown(UP_ARROW) && player.index !== null){
-      player.distance +=10
+    if(keyIsDown(UP_ARROW) && player.index !== null ){
+      player.distance +=20;
+      
       player.update();
     }
-
+if (player.distance >4000){
+  gameState = 2;
+}
     drawSprites();
+  }
+  end(){
+    game.update(2);
+    console.log(gameState);
   }
 }
--- sketch.js
+++ sketch.js
@@ -8,8 +8,15 @@ var database;
 
 var form, player, game;
 
-var cars, car1, car2, car3, car4;
-
+var cars, car1, car2, car3, car4,car1Image,car2Image,car3Image,car4Image,trackImage,groundImage;
+function preload(){
+car1Image = loadImage("../images/car1.png");
+car2Image = loadImage("../images/car2.png");
+car3Image = loadImage("../images/car3.png");
+car4Image = loadImage("../images/car4.png");
+trackImage = loadImage("../images/track.jpg");
+groundImage = loadImage("../images/ground.png");
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
@@ -21,11 +28,15 @@ function setup(){
 
 
 function draw(){
-  if(playerCount === 4){
+
+  if(playerCount === 4 && gameState ===0){
     game.update(1);
   }
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState === 2){
+    game.end();    
   }
 }
